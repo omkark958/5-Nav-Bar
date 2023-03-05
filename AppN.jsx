5588f69a9a72c96d3a839990a4c6cf1de8a59ba5
omkark958 <126230302+omkark958@users.noreply.github.com>
2023-03-05 21:36:45 +0530

    Add files via upload

---
+++ AppN.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Nav from "./NavBarF/Nav";
+import "./global1.css"
+import "./global2.css"
+import "./global3.css"
+import "./global4.css"
+import "./global5.css"
+import Demo from "./Demo";
+
+
+const AppN=()=>{
+    return(
+        <>
+        <Nav/>
+        <Demo/>
+        </>
+    )
+}
+export default AppN
---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import {createRoot} from "react-dom/client"
+import AppN from "./AppN";
+// import Demo from "./Demo";
+
+createRoot(document.getElementById("root")).render(<AppN/>)
+// createRoot(document.getElementById("root")).render(<Demo/>)
